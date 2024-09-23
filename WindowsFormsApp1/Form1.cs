e291e416e219b2d441e2b2d52f4ff1f3864979a2
EaglePancake <eaglepancake@gmail.com>
2024-09-23 11:48:50 +0300

    Initial commit

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int s1, s2, sonuc;
+            s1 = Convert.ToInt32(textBox1.Text);
+            s2 = Convert.ToInt32(textBox2.Text);
+
+            sonuc = s1 + s2;
+
+            MessageBox.Show($"Sonuç: {sonuc}");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("İlk program!!!");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
